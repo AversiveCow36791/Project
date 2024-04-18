416db249da9828932a87bb76b086db2ff9016d7d
Anthony <138241382+AversiveCow36791@users.noreply.github.com>
2024-04-17 22:05:57 -0400

    sign up commit

--- src/Users/Signup.tsx
+++ src/Users/Signup.tsx
@@ -15,13 +15,15 @@ export default function Signup() {
   };
   return (
     <div>
-      <h1>Signup</h1>
-      {error && <div>{error}</div>}
+      <h1>AccountSignup</h1>
+      <span style={{color:"red"}}>{error && <div>{error}</div>}</span>
+      Username:
       <input value={user.username} onChange={(e) => setUser({
-          ...user, username: e.target.value })} />
+          ...user, username: e.target.value })} className="mb-1 ms-1" />
+      Password:
       <input value={user.password} onChange={(e) => setUser({
-          ...user, password: e.target.value })} />
-      <button onClick={signup}> Signup </button>
+          ...user, password: e.target.value })} className="ms-2" />
+      <button onClick={signup} className="btn btn-warning mt-1"> Signup </button>
     </div>
   );
 }
