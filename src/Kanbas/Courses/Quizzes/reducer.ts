cb6db0188806a71607d126ae4b1afa933d0eeacf
Hudson7777 <1155124383@link.cuhk.edu.hk>
2024-04-19 13:53:11 -0400

    add services and reducer for quizzes

---
+++ src/Kanbas/Courses/Quizzes/reducer.ts
@@ -0,0 +1,51 @@
+// quizzesSlice.ts
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    quizzes: [] as {
+      _id: string; title: string; dueDate: string; QuizType: string; Points: number; numOfQuestions: number;
+      questions: { id: string; question: string; options: string[]; correctAnswer: string }[];
+      published: boolean; course: string;
+    }[],
+    quiz: {
+      title: "New Quiz",
+      dueDate: "2023-01-01",
+      QuizType: 'Graded Quiz',
+      Points: 0,
+      numOfQuestions: 0,
+      questions: [],
+      published: false,
+      course: '', // assuming course can be added here for context
+    }, 
+  };
+
+const quizzesSlice = createSlice({
+  name: 'quizzes',
+  initialState,
+  reducers: {
+    setQuizzes: (state, action) => {
+      state.quizzes = action.payload;
+    },
+    addQuiz: (state, action) => {
+      state.quizzes = [action.payload, ...state.quizzes];
+    },
+    deleteQuiz: (state, action) => {
+      state.quizzes = state.quizzes.filter((quiz) => quiz._id !== action.payload);
+    },
+    updateQuiz: (state, action) => {
+      state.quizzes = state.quizzes.map((quiz) => {
+        if (quiz._id === action.payload._id) {
+          return action.payload;
+        } else {
+          return quiz;
+        }
+      });
+    },
+    setQuiz: (state, action) => {
+      state.quiz = action.payload;
+    },
+  },
+});
+
+export const { addQuiz, deleteQuiz, updateQuiz, setQuiz, setQuizzes } = quizzesSlice.actions;
+export default quizzesSlice.reducer;
---
+++ src/Kanbas/Courses/Quizzes/services.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:4000";
+const COURSES_API = `${API_BASE}/api/courses`;
+const QUIZZES_API = `${API_BASE}/api/quizzes`;
+
+export const updateQuiz = async (quiz: any) => {
+  const response = await axios.put(`${QUIZZES_API}/${quiz._id}`, quiz);
+  return response.data;
+};
+
+export const deleteQuiz = async (quizId: any) => {
+  const response = await axios.delete(`${QUIZZES_API}/${quizId}`);
+  return response.data;
+};
+
+export const createQuiz = async (courseId: any, quiz: any) => {
+  const response = await axios.post(`${COURSES_API}/${courseId}/quizzes`, quiz);
+  return response.data;
+};
+
+export const findQuizzesForCourse = async (courseId: any) => {
+  const response = await axios.get(`${COURSES_API}/${courseId}/quizzes`);
+  return response.data;
+};
