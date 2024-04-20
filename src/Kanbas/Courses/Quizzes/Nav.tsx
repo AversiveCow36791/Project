e56bc3659f9ad6d33bef85c7b9cb8bb722eb9371
Anthony <antlee134@gmail.com>
2024-04-20 13:11:47 -0400

    testing

--- src/Kanbas/Courses/Quizzes/Nav.tsx
+++ src/Kanbas/Courses/Quizzes/Nav.tsx
@@ -3,7 +3,7 @@ function Nav() {
   const { pathname } = useLocation();
  return (
    <nav className="nav nav-tabs mt-2">
-     <Link className={`nav-link ${pathname.includes("Details") ? "active" : ""}`} to="" style={{color:"red"}}>Details</Link>
+     <Link className={`nav-link ${pathname.includes("Details") || pathname === "/Editor/" ? "active" : ""}`} to="" style={{color:"red"}}>Details</Link>
      <Link className={`nav-link ${pathname.includes("Questions") ? "active" : ""}`} to="" style={{color:"red"}}>Questions</Link>
    </nav>
  );
